a453bc84c3e4b1cd62e5522ad6a8efeb41060e5d
pratikshabathe <pratikshabathe11@gmail.com>
2022-11-18 21:22:09 +0530

    create new branch

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -5,21 +5,35 @@ import java.util.Random;
 public class EmployeeWage {
 
 	public static void main(String[] args) {
-		System.out.println("--Welcome to employee wage ");
-		
-		String attendanceString = EmployeeWage.checkAttendance();
-	    System.out.println(attendanceString);
-	    checkAttendance() ;
+	    System.out.println("--Welcome to Employee Wage Computation Program--\n");
 
-	}
+	    String attendanceString = EmployeeWage.checkAttendance();
+	    System.out.println(attendanceString);
+
+	    System.out.println("Employee's today's wage: " + getDailyWage(attendanceString) + "$");
+
+	  }
 
 	  private static String checkAttendance() {
 	    Random random = new Random();
 	    boolean randomBool = random.nextBoolean();
 
 	    if (randomBool) return "Employee is present.";
-	    
 	    else return "Employee is absent.";
 
-	}
+	  }
+
+	  private static int getDailyWage(String str) {
+
+	   boolean isPresent = str.contains("present") || str.contains("Present");
+
+	   int empRatePerHr = 20;
+	   int hrsWorked = 0;
+
+	   if (isPresent) hrsWorked = 8;
+	   int dailyWage = empRatePerHr * hrsWorked;
+	   return dailyWage;
+	   
+	  }
+	  
 }
